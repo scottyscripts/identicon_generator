70df2fe44f745f25dabe0c6790a98ca71881372d
sdavidson140 <sdavidson140@gmail.com>
2019-03-16 14:54:45 -0400

    define function to build 5x5 grid for identicon

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -3,10 +3,21 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_color
+    |> build_grid
   end
 
   def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
     %Identicon.Image{image | color: {r, g, b}}
+  end
+
+  def build_grid(%Identicon.Image{hex: hex_list} = image) do
+    hex_list
+    |> Enum.chunk_every(3, 3, :discard)
+    |> Enum.map(&mirror_row/1)
+  end
+
+  def mirror_row([first, second | _tail] = row) do
+    row ++ [second, first]
   end
 
   @doc """
