0f1f52b7c8e6107972076c775734ba369da21da1
sdavidson140 <sdavidson140@gmail.com>
2019-03-17 11:50:12 -0400

    add documentation

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,8 +1,15 @@
 defmodule Identicon do
+  @moduledoc """
+  This module is used to convert any input string into an identicon image.
+  """
   require Integer
+
+  @doc """
+  Takes an input string as an argument, converts it to an identicon image, and saves locally.
+  """
   def main(input) do
     input
-    |> hash_input
+    |> hash_input_str
     |> pick_color
     |> build_grid
     |> filter_odd_squares
@@ -11,10 +18,40 @@ defmodule Identicon do
     |> save_image(input)
   end
 
+  @doc """
+  Hashes an input string using md5 algorithm.
+  Converts new hash to list.
+
+  Creates new `%Identicon.Image` struct and saves hash list to `hex` field.
+  """
+  def hash_input_str(input) do
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
+  end
+
+  @doc """
+  Determines and sets value for `color` field of `%Identicon.Image` struct.
+
+  Uses first 3 values from list generated in `hash_input_str/1`
+  """
   def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
     %Identicon.Image{image | color: {r, g, b}}
   end
 
+
+  @doc """
+  Creates list of tuples representing 5x5 grid using `%Identicon.Image`'s' `hex` value.
+
+  Each tuple consists of the original hex value and its index.
+
+  Indices map to resulting grid as follows
+      [ 0,  1,  2,  3,  4
+        5,  6,  7,  8,  9,
+        10, 11, 12, 13, 14 ]
+
+  """
   def build_grid(%Identicon.Image{hex: hex_list} = image) do
     grid =
       hex_list
@@ -26,10 +63,25 @@ defmodule Identicon do
     %Identicon.Image{image | grid: grid}
   end
 
+  @doc """
+  Takes a list of 3 values and mirrors them to create a list w/ 5 values
+
+  ## Examples
+
+      iex> list = [455, 287, 343]
+      iex> Identicon.mirror_row(list)
+      [455, 287, 343, 287, 455]
+
+  """
   def mirror_row([first, second | _tail] = row) do
     row ++ [second, first]
   end
 
+  @doc """
+  Removes all tuples from list stored in `%Identicon.Image` grid.
+
+  Keeps tuples containing even hex values, since these will be colored in final identicon.
+  """
   def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
     filtered_grid =
       grid
@@ -40,6 +92,12 @@ defmodule Identicon do
     %Identicon.Image{image | grid: filtered_grid}
   end
 
+  @doc """
+  Generates pixel_map on `%Identicon.Image`.
+
+  pixel_map is a list of tuples containing start (top left) and stop (bottom right)
+  coordinates when drawing colors on identicon image.
+  """
   def build_pixel_map(%Identicon.Image{grid: grid} = image) do
     pixel_map =
       grid
@@ -56,6 +114,10 @@ defmodule Identicon do
       %Identicon.Image{image | pixel_map: pixel_map}
   end
 
+  @doc """
+  Uses erlang's :egd (erlang graphical drawer) to create an image and color in values
+  determined by `build_pixel_map/1`
+  """
   def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
     image = :egd.create(250, 250)
     fill = :egd.color(color)
@@ -73,18 +135,13 @@ defmodule Identicon do
     :egd.render(image)
   end
 
-  def save_image(image, input) do
-    File.write("#{input}.png", image)
-  end
   @doc """
-  generates `Identicon.Image` struct created from list of bytes
+  Saves newly generated identicon locally.
 
-  `input` is a string that gets hashed using md5 algorithm
+  File name determined by original input string passed to `main/1`
   """
-  def hash_input(input) do
-    hex = :crypto.hash(:md5, input)
-    |> :binary.bin_to_list
-
-    %Identicon.Image{hex: hex}
+  def save_image(image, input) do
+    File.write("images/#{input}.png", image)
   end
+
 end
