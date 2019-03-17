b1eb76ae49876fbf37870270bf067d3b4f533e1b
sdavidson140 <sdavidson140@gmail.com>
2019-03-17 11:23:05 -0400

    draw & save identicon png image - add erlang/egd to deps

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -7,6 +7,8 @@ defmodule Identicon do
     |> build_grid
     |> filter_odd_squares
     |> build_pixel_map
+    |> draw_image
+    |> save_image(input)
   end
 
   def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
@@ -45,13 +47,34 @@ defmodule Identicon do
         horizontal_origin = rem(index, 5) * 50
         vertical_origin = div(index, 5) * 50
 
-        top_left = {horizontal, vertical}
-        bottom_right = {horizontal + 50, vertical + 50}
+        top_left = {horizontal_origin, vertical_origin}
+        bottom_right = {horizontal_origin + 50, vertical_origin + 50}
 
         {top_left, bottom_right}
       end)
 
       %Identicon.Image{image | pixel_map: pixel_map}
+  end
+
+  def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
+    image = :egd.create(250, 250)
+    fill = :egd.color(color)
+
+    pixel_map
+    |> Enum.each(fn({top_left, bottom_right}) ->
+      :egd.filledRectangle(
+        image,
+        top_left,
+        bottom_right,
+        fill
+      )
+      end)
+
+    :egd.render(image)
+  end
+
+  def save_image(image, input) do
+    File.write("#{input}.png", image)
   end
   @doc """
   generates `Identicon.Image` struct created from list of bytes
